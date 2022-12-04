66c7f0cfc981c5ca1a842ff627d1a9336522f639
Matthew Hart <mattevanhart@gmail.com>
2022-12-04 16:17:20 -0500

    day 4. convert text to type asap

--- 2022/4/solve.py
+++ 2022/4/solve.py
@@ -3,6 +3,21 @@ with open("./input.txt") as file:
   data = file.read().splitlines()
 
 def solve():
-  pass
+  containCount = 0
+  overlapCount = 0
+  for pair in data:
+    ass = pair.split(",")
+    
+    rangeBounds0 = ass[0].split("-")
+    rangeBounds1 = ass[1].split("-")
+
+    if int(rangeBounds0[0]) <= int(rangeBounds1[0]) and int(rangeBounds0[1]) >= int(rangeBounds1[1]) or int(rangeBounds0[0]) >= int(rangeBounds1[0]) and int(rangeBounds0[1]) <= int(rangeBounds1[1]):
+      containCount += 1
+
+    if int(rangeBounds0[0]) <= int(rangeBounds1[1]) and int(rangeBounds0[1]) >= int(rangeBounds1[0]):
+      overlapCount += 1
+
+  print(containCount)
+  print(overlapCount)
 
 solve()
