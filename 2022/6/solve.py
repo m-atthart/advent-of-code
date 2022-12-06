8f59524856bf0903a60ed3b67d4569ee740f4042
Matthew Hart <mattevanhart@gmail.com>
2022-12-06 00:24:15 -0500

    day 6. easy

--- 2022/6/solve.py
+++ 2022/6/solve.py
@@ -1,8 +1,20 @@
 data = []
 with open("./input.txt") as file:
-  data = file.read().splitlines()
+  data = file.read().splitlines()[0]
 
 def solve():
-  pass
+  i = 0
+  window = data[i:i+14]
+  window_count = {}
+  for char in window:
+    window_count[char] = 1
+  while len(window_count.keys()) != 14:
+    i += 1
+    window = data[i:i+14]
+    window_count = {}
+    for char in window:
+      window_count[char] = 1
+  print(window)
+  print(i+14)
 
 solve()
